675ee3b4016fe711c29a66614fd5372fa0888c34
Todd Wilson <todd@toddwilson.net>
2013-12-24 16:41:38 -0500

    enable delayed_job

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -4,6 +4,8 @@
 #  }
 #end
 
+include_recipe "delayed_job"
+
 # uncomment to turn on thinking sphinx 2/ultra sphinx. Remember to edit cookbooks/sphinx/recipes/default.rb first!
 # include_recipe "sphinx"
 
